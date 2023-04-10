d05b418686f942e93bfe682e580484577b90bcef
chechna9 <ja_bouchouareb@esi.dz>
2023-04-10 01:27:08 +0100

    respo fixed in certeficates

--- src/sections/certeficates.tsx
+++ src/sections/certeficates.tsx
@@ -12,32 +12,32 @@ const Certeficates = () => {
   const certeficateList = useCerteficates();
   return (
     <div className="bg-prim2">
-      <div className="bg-prim1 min-h-screen  py-10 md:ml-10 ml-5 md:rounded-bl-[200px] rounded-bl-[100px] md:rounded-tl-[200px] rounded-tl-[100px]">
-      <div className="relative w-fit h-fit mb-8 mx-auto">
-         
+      <div className="bg-prim1 h-screen md:ml-10 ml-5 md:rounded-bl-[200px] rounded-bl-[100px] md:rounded-tl-[200px] rounded-tl-[100px] relative">
+        <div className="relative w-fit h-fit mb-8 mx-auto top-3">
           <h1 className="text-white font-bold  text-4xl ">Certeficates</h1>
-          <img
-            src={brain}
-            alt=""
-            className="h-5 absolute -left-6 top-0"
-          />
+          <img src={brain} alt="" className="h-5 absolute -left-6 top-0" />
         </div>
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={30}
-          freeMode={true}
-          modules={[FreeMode]}
-        >
-          {certeficateList.map((page) => (
-            <SwiperSlide>
-              <div className="grid grid-rows-2 grid-flow-col">
-                {page.map((value, index) => (
-                  <Certeficate imgPath={value.imgPath} key={index} />
-                ))}
-              </div>{" "}
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        
+          <div   className="absolute bottom-[50%] translate-y-[50%] w-full">
+            <Swiper 
+            
+              slidesPerView={1}
+              spaceBetween={30}
+              freeMode={true}
+              modules={[FreeMode]}
+            >
+              {certeficateList.map((page) => (
+                <SwiperSlide>
+                  <div className="grid md:grid-rows-2 grid-rows-3 grid-flow-col">
+                    {page.map((value, index) => (
+                      <Certeficate imgPath={value.imgPath} key={index} />
+                    ))}
+                  </div>{" "}
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+       
       </div>
     </div>
   );
