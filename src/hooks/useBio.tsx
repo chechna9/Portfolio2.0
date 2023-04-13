58550741ab3ad7029004ea223c3f88c1083a176d
chechna9 <ja_bouchouareb@esi.dz>
2023-04-13 13:56:39 +0100

    ignored dependancy in useEffect

--- src/hooks/useBio.tsx
+++ src/hooks/useBio.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 
 interface Bio {
@@ -169,7 +170,7 @@ const useBio = (): Bio => {
   };
   useEffect(() => {
     fillIt();
-  }, );
+  }, []);
 
   return bio;
 };
