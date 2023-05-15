ec49749747d2c76fe6d5c3187b91d077215796c6
chechna9 <ja_bouchouareb@esi.dz>
2023-05-15 21:14:00 +0100

    removed some certeficates

--- src/data/certeficates.ts
+++ src/data/certeficates.ts
@@ -26,28 +26,9 @@ let data = [
   },
 
   {
-    img: "https://res.cloudinary.com/dmgeaamqy/image/upload/v1681448382/portfolioAssets/certeficates/Ahmed_Yacine_Bouchouareb_-_Python_nxchvz.png",
-  },
-  {
-    img: "https://res.cloudinary.com/dmgeaamqy/image/upload/v1681448368/portfolioAssets/certeficates/Ahmed_Yacine_Bouchouareb_-_Intermediate_Machine_Learning_zv1gmq.png",
-  },
-
-  {
-    img: "https://res.cloudinary.com/dmgeaamqy/image/upload/v1681448373/portfolioAssets/certeficates/Ahmed_Yacine_Bouchouareb_-_Intro_to_Machine_Learning_1_sxsqmj.png",
-  },
-  {
     img: "https://res.cloudinary.com/dmgeaamqy/image/upload/v1681448411/portfolioAssets/certeficates/superVisedSklearn_x3eree.png",
   },
 
-  {
-    img: "https://res.cloudinary.com/dmgeaamqy/image/upload/v1681448403/portfolioAssets/certeficates/understandingDataScience_bapeiq.png",
-  },
-  {
-    img: "https://res.cloudinary.com/dmgeaamqy/image/upload/v1681448377/portfolioAssets/certeficates/AiFund_iz5tph.png",
-  },
-  {
-    img: "https://res.cloudinary.com/dmgeaamqy/image/upload/v1681448369/portfolioAssets/certeficates/Ahmed_Yacine_Bouchouareb_-_Pandas_1_ptkmex.png",
-  },
   {
     img: "https://res.cloudinary.com/dmgeaamqy/image/upload/v1682970318/portfolioAssets/certeficates/ts_analysis_vd55zu.png",
   },
